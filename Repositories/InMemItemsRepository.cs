1ac24ab33cc07f9fbf24f4c0bdae6257bd2cb7f4
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-10-25 17:57:14 +0600

    POST,PUT,DELETE action for item

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -25,6 +25,21 @@ namespace Catalog.Repositories
             return items.Where(item => item.Id == id).SingleOrDefault();
         }
 
+        public void CreateItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var index=items.FindIndex(exixtingItem=>exixtingItem.Id==item.Id);
+            items[index]=item; 
+        }
+
+        public void DeleteItem(Guid id){
+            var index=items.FindIndex(existingItem=>existingItem.Id==id);
+            items.RemoveAt(index);
+        }
     }
 
 
