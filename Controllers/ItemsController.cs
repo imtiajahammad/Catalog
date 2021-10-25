21cd9de0959dd5a5efa7d2d9738b39b4f0aba6eb
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-10-25 15:56:05 +0600

    DependencyInjectionAndDTOs-2

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
 using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -11,28 +13,28 @@ namespace Catalog.Controllers{
     [Route("[controller]")]
     public class ItemsController:ControllerBase{
 
-        private readonly InMemItemsRepository repository;
-        public ItemsController(){
-            repository=new InMemItemsRepository();
+        private readonly IItemsRepository _repository;
+        public ItemsController(IItemsRepository repository){
+            _repository=repository;
         }
 
 
         // GET /items
         [HttpGet]
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-            var items=repository.GetItems();
+            var items=_repository.GetItems().Select(item=>item.AsDto());
             return items;
         }
 
         // GET/ items/{id}
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id){
-            var item=repository.GetItem(id);
+        public ActionResult<ItemDto> GetItem(Guid id){
+            var item=_repository.GetItem(id);
             if(item==null){
                 return NotFound();
             }
-            return item;
+            return item.AsDto();
         }
         
 
