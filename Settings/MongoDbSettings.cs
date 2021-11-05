2937c81792556ddf86d97e443af8adcfe8dfb328
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-11-05 12:54:01 +0600

    db with user idPass, pass in UserSecrets

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -4,11 +4,19 @@ namespace Catalog.Settings{
 
         public string Host { get; set; }
         public int Port { get; set; }
+
+        public string User { get; set; }
+        public string Password { get; set; }
+        //public string ReplicaSetName { get; set; }
         public string ConnectionString 
         {
              get
              {
-                 return $"mongodb://{Host}:{Port}";
+                 var aa=$"mongodb://{User}:{Password}@{Host}:{Port}";
+                 return aa;//$"mongodb://{Host}:{Port}";
+                 //return $"mongodb://{User}:{Password}@{Host}:{Port}/mongo";
+                 //return $"mongodb://{User}:{Password}@{Host}:{Port}";
+                 //mongodb://myusername:mypassword@serverip:27017/databasename
              } 
              
         }
--- obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1db75671-944f-4601-8f92-92ba3231a825")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
